838ef8d6949e78d301c86071328befbef5f40c1e
Sheriff-Jimoh <56472884+Sheriff-Jimoh@users.noreply.github.com>
2021-04-20 15:52:35 +0200

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -83,7 +83,7 @@ $(window).on('load', function() {
    * Loads the basemap and adds it to the map
    */
   function addBaseMap() {
-    var basemap = trySetting('_tileProvider', 'Stamen.TonerLite');
+    var basemap = trySetting('_tileProvider', 'OpenStreetMap_Mapnik');
     L.tileLayer.provider(basemap, {
       maxZoom: 18
     }).addTo(map);
