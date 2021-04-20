3278a4fcdbd1e94eb9fd859a0fbb270805dfb731
Sheriff-Jimoh <56472884+Sheriff-Jimoh@users.noreply.github.com>
2021-04-20 15:10:16 +0200

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -474,7 +474,7 @@ $(window).on('load', function() {
     var credit = 'Map <a href="'
       // Show Google Sheet URL if the variable exists and is not empty, otherwise link to Chapters.csv
       + (typeof googleDocURL !== 'undefined' && googleDocURL ? googleDocURL : './csv/Chapters.csv')
-      + '" target="_blank">data</a> and photos by';
+      + '" target="_blank">Application</a> created';
 
     var name = getSetting('_authorName');
     var web = getSetting('_webDeveloper')
